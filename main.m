c33997f8a7eede46f31eb9977a57c5bb2b90f973
Tom Fishwick <fishwickt@gmail.com>
2021-11-21 21:21:15 +0000

    Update main.m
    Got mode and histograms

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-clc;
+clc; clear; close all;
 %% Load the data
 data = readtable('data/ford.csv');
 
@@ -29,7 +29,21 @@ disp("Post-Changes:");
 disp(stats2);
 
 % Get the mode for non number columns
+disp(array2table(["Model", char(mode(data.model));
+    "Transmission", char(mode(data.transmission));
+    "Fuel Type", char(mode(data.fuelType))], 'VariableNames', ["Column", "Mode"])); 
+% https://stackoverflow.com/questions/32341083/extract-string-from-categorical-variable-in-matlab
+% for how to get the string value of a categorical
 
 % Histograms
+f = figure;
+f.Position = [50, 50, 960,780];
+for i = 1: width(data)
+    %figure
+    subplot(width(data),1,i);
+    histogram(data{:, i});
+    title(data.Properties.VariableNames(i));
+    ylabel("Count");
+end
 
 % Graph against price
